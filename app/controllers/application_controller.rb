eb5658d26adbd0b3a420fcbfdc6657053437df55
Marcin Grzywaczewski <killavus@gmail.com>
2016-03-10 16:47:34 +0100

    Do not verify CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
   protected
   def render_error(exc)
     render json: { errors: { message: exc.message } }, status: :unprocessable_entity
